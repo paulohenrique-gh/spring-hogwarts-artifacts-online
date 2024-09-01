11094a8fe2db36cb00e24a7e5227ea0b919dba2c
Paulo Henrique <ph.1928@hotmail.com>
2024-09-01 09:05:17 -0300

    First commit Episode 12 - https://www.youtube.com/watch?v=W7xJf5Wknho&list=PLqq9AhcMm2oPdXXFT3fzjaKLsVymvMXaY&index=12&t=114s

---
+++ src/main/java/com/learningspring/hogwartsartifactonline/HogwartsArtifactOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.learningspring.hogwartsartifactonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HogwartsArtifactOnlineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HogwartsArtifactOnlineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/learningspring/hogwartsartifactonline/HogwartsArtifactOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learningspring.hogwartsartifactonline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HogwartsArtifactOnlineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
