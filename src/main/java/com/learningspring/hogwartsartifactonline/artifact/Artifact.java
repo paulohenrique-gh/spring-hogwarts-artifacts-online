4987a4470d85d92f01072a2af7b9eac55fbfe181
Paulo Henrique <ph.1928@hotmail.com>
2024-09-02 10:58:49 -0300

    feat: create Artifact and Wizard model classes
    Episode 16: https://www.youtube.com/watch?v=bSLXER_84i8&list=PLqq9AhcMm2oPdXXFT3fzjaKLsVymvMXaY&index=16&pp=iAQB

---
+++ src/main/java/com/learningspring/hogwartsartifactonline/artifact/Artifact.java
@@ -0,0 +1,68 @@
+package com.learningspring.hogwartsartifactonline.artifact;
+
+import com.learningspring.hogwartsartifactonline.wizard.Wizard;
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+
+@Entity
+public class Artifact implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    private String name;
+
+    private String description;
+
+    private String imageUrl;
+
+    @ManyToOne
+    private Wizard owner;
+
+    public Artifact() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public Wizard getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Wizard owner) {
+        this.owner = owner;
+    }
+}
---
+++ src/main/java/com/learningspring/hogwartsartifactonline/wizard/Wizard.java
@@ -0,0 +1,47 @@
+package com.learningspring.hogwartsartifactonline.wizard;
+
+import com.learningspring.hogwartsartifactonline.artifact.Artifact;
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.util.List;
+
+@Entity
+public class Wizard implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    private String name;
+
+    @OneToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE}, mappedBy = "owner")
+    private List<Artifact> artifacts;
+
+    public Wizard() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Artifact> getArtifacts() {
+        return artifacts;
+    }
+
+    public void setArtifacts(List<Artifact> artifacts) {
+        this.artifacts = artifacts;
+    }
+}
