585178b094f348bf41a81cb853a20b17e220e897
Paulo Henrique <ph.1928@hotmail.com>
2024-09-05 10:51:17 -0300

    fix: strategy for generated value in Wizard and Artifact
    Episode 21: https://www.youtube.com/watch?v=BM7TjjJinXA&list=PLqq9AhcMm2oPdXXFT3fzjaKLsVymvMXaY&index=21

--- src/main/java/com/learningspring/hogwartsartifactonline/artifact/Artifact.java
+++ src/main/java/com/learningspring/hogwartsartifactonline/artifact/Artifact.java
@@ -11,7 +11,6 @@ public class Artifact implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
 
     private String name;
--- src/main/java/com/learningspring/hogwartsartifactonline/wizard/Wizard.java
+++ src/main/java/com/learningspring/hogwartsartifactonline/wizard/Wizard.java
@@ -10,7 +10,7 @@ import java.util.List;
 public class Wizard implements Serializable {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
 
     private String name;
