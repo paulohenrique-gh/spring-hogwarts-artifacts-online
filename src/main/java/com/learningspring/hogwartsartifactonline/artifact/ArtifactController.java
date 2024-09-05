972e2f691e939405d9ac5e1ec953b79287d7fcf7
Paulo Henrique <ph.1928@hotmail.com>
2024-09-05 10:51:46 -0300

    feat: implement ArtifactController#findArtifactById
    Episode 21: https://www.youtube.com/watch?v=BM7TjjJinXA&list=PLqq9AhcMm2oPdXXFT3fzjaKLsVymvMXaY&index=21

--- src/main/java/com/learningspring/hogwartsartifactonline/artifact/ArtifactController.java
+++ src/main/java/com/learningspring/hogwartsartifactonline/artifact/ArtifactController.java
@@ -1,11 +1,14 @@
 package com.learningspring.hogwartsartifactonline.artifact;
 
 import com.learningspring.hogwartsartifactonline.system.Result;
+import com.learningspring.hogwartsartifactonline.system.StatusCode;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/api/v1/artifacts")
+@RestController
+@RequestMapping("/api/v1/artifacts")
 public class ArtifactController {
 
     private final ArtifactService artifactService;
@@ -16,6 +19,7 @@ public class ArtifactController {
 
     @GetMapping("/{artifactId}")
     public Result findArtifactById(@PathVariable String artifactId) {
-        return null;
+        Artifact foundArtifact = this.artifactService.findById(artifactId);
+        return new Result(true, StatusCode.SUCCESS, "Find One Success", foundArtifact);
     }
 }
