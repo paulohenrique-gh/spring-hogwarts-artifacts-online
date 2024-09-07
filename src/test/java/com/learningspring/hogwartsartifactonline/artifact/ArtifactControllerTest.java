a98be80e759f723a81f697fb6f7def5f809b374b
Paulo Henrique <ph.1928@hotmail.com>
2024-09-07 09:57:52 -0300

    test: add testFindAllArtifactsSuccess for ArtifactController
    Episode 24: https://www.youtube.com/watch?v=BM7TjjJinXA&list=PLqq9AhcMm2oPdXXFT3fzjaKLsVymvMXaY&index=234

--- src/test/java/com/learningspring/hogwartsartifactonline/artifact/ArtifactControllerTest.java
+++ src/test/java/com/learningspring/hogwartsartifactonline/artifact/ArtifactControllerTest.java
@@ -1,6 +1,7 @@
 package com.learningspring.hogwartsartifactonline.artifact;
 
 import com.learningspring.hogwartsartifactonline.system.StatusCode;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -109,4 +110,22 @@ class ArtifactControllerTest {
                 .andExpect(jsonPath("$.message").value("Could not find artifact with Id 1250808601744904191 :("))
                 .andExpect(jsonPath("$.data").isEmpty());
     }
+
+    @Test
+    void testFindAllArtifactsSuccess() throws Exception {
+        // Given
+        given(this.artifactService.findAll()).willReturn(this.artifacts);
+
+        // When and then
+        this.mockMvc.perform(get("/api/v1/artifacts").accept(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.flag").value(true))
+                .andExpect(jsonPath("$.code").value(StatusCode.SUCCESS))
+                .andExpect(jsonPath("$.message").value("Find All Success"))
+                .andExpect(jsonPath("$.data", Matchers.hasSize(this.artifacts.size())))
+                .andExpect(jsonPath("$.data[0].id").value("1250808601744904191"))
+                .andExpect(jsonPath("$.data[0].name").value("Deluminator"))
+                .andExpect(jsonPath("$.data[1].id").value("1250808601744904192"))
+                .andExpect(jsonPath("$.data[1].name").value("Invisibility Cloak"));
+    }
 }
+
