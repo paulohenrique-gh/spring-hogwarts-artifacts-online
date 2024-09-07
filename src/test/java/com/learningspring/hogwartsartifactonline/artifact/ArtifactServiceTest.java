62aa1fe507acf16830101fbd18d33da6f80ef18a
Paulo Henrique <ph.1928@hotmail.com>
2024-09-07 09:59:31 -0300

    chore: remove unused import
    Episode 24: https://www.youtube.com/watch?v=BM7TjjJinXA&list=PLqq9AhcMm2oPdXXFT3fzjaKLsVymvMXaY&index=234

--- src/test/java/com/learningspring/hogwartsartifactonline/artifact/ArtifactServiceTest.java
+++ src/test/java/com/learningspring/hogwartsartifactonline/artifact/ArtifactServiceTest.java
@@ -12,7 +12,6 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.NoSuchElementException;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
