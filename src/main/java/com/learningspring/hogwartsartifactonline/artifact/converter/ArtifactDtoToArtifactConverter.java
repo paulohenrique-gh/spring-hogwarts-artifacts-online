144a8dcd06d6ff549ea5c2e84d1f338f00345289
Paulo Henrique <ph.1928@hotmail.com>
2024-09-08 11:32:09 -0300

    feat: create ArtifactDtoToArtifactConverter
    Episode 25: https://www.youtube.com/watch?v=BM7TjjJinXA&list=PLqq9AhcMm2oPdXXFT3fzjaKLsVymvMXaY&index=25

---
+++ src/main/java/com/learningspring/hogwartsartifactonline/artifact/converter/ArtifactDtoToArtifactConverter.java
@@ -0,0 +1,19 @@
+package com.learningspring.hogwartsartifactonline.artifact.converter;
+
+import com.learningspring.hogwartsartifactonline.artifact.Artifact;
+import com.learningspring.hogwartsartifactonline.artifact.dto.ArtifactDto;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ArtifactDtoToArtifactConverter implements Converter<ArtifactDto, Artifact> {
+    @Override
+    public Artifact convert(ArtifactDto source) {
+        Artifact artifact = new Artifact();
+        artifact.setId(source.id());
+        artifact.setName(source.name());
+        artifact.setDescription(source.description());
+        artifact.setImageUrl(source.imageUrl());
+        return artifact;
+    }
+}
