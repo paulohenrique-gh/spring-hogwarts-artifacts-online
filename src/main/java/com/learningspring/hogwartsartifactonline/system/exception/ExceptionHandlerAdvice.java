cce0841f0f4b0341cce22378c36b936c42e9be9a
Paulo Henrique <ph.1928@hotmail.com>
2024-09-08 11:33:35 -0300

    feat: add validation exception handler
    Episode 25: https://www.youtube.com/watch?v=BM7TjjJinXA&list=PLqq9AhcMm2oPdXXFT3fzjaKLsVymvMXaY&index=25

--- src/main/java/com/learningspring/hogwartsartifactonline/system/exception/ExceptionHandlerAdvice.java
+++ src/main/java/com/learningspring/hogwartsartifactonline/system/exception/ExceptionHandlerAdvice.java
@@ -4,9 +4,16 @@ import com.learningspring.hogwartsartifactonline.artifact.ArtifactNotFoundExcept
 import com.learningspring.hogwartsartifactonline.system.Result;
 import com.learningspring.hogwartsartifactonline.system.StatusCode;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestControllerAdvice
 public class ExceptionHandlerAdvice {
@@ -17,4 +24,23 @@ public class ExceptionHandlerAdvice {
         return new Result(false, StatusCode.NOT_FOUND, ex.getMessage());
     }
 
+
+    /**
+     * This handles invalid inputs
+     *
+     * @param ex
+     * @return Result
+     */
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    Result handleValidationException(MethodArgumentNotValidException ex) {
+        List<ObjectError> errors = ex.getBindingResult().getAllErrors();
+        Map<String, String> map = new HashMap<>(errors.size());
+        errors.forEach(error -> {
+            String key = ((FieldError) error).getField();
+            String val = error.getDefaultMessage();
+            map.put(key, val);
+        });
+        return new Result(false, StatusCode.INVALID_ARGUMENT, "Provided arguments are invalid, see data for details.", map);
+    }
 }
