b581e1dc302566dbb9770bc301ca0ada57b05d45
Paulo Henrique <ph.1928@hotmail.com>
2024-09-08 11:32:39 -0300

    chore: add idWorker bean to spring context
    Episode 25: https://www.youtube.com/watch?v=BM7TjjJinXA&list=PLqq9AhcMm2oPdXXFT3fzjaKLsVymvMXaY&index=25

--- src/main/java/com/learningspring/hogwartsartifactonline/HogwartsArtifactOnlineApplication.java
+++ src/main/java/com/learningspring/hogwartsartifactonline/HogwartsArtifactOnlineApplication.java
@@ -1,13 +1,20 @@
 package com.learningspring.hogwartsartifactonline;
 
+import com.learningspring.hogwartsartifactonline.artifact.utils.IdWorker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class HogwartsArtifactOnlineApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(HogwartsArtifactOnlineApplication.class, args);
+    }
+
+    @Bean
+    public IdWorker idWorker() {
+        return new IdWorker(1, 1);
     }
 
 }
