7026865bf88a7628402c0cb0f55d5f659f067c97
Paulo Henrique <ph.1928@hotmail.com>
2024-09-11 10:53:59 -0300

    feat: create WizardController
    Episode 29: https://www.youtube.com/watch?v=BM7TjjJinXA&list=PLqq9AhcMm2oPdXXFT3fzjaKLsVymvMXaY&index=29

---
+++ src/main/java/com/learningspring/hogwartsartifactonline/wizard/WizardController.java
@@ -0,0 +1,11 @@
+package com.learningspring.hogwartsartifactonline.wizard;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("apí/v1/wizards")
+public class WizardController {
+
+
+}
