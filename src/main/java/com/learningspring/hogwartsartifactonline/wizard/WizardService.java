1aa7e97184855405f1f70a2c1ac0dc65cae1a46a
Paulo Henrique <ph.1928@hotmail.com>
2024-09-11 10:55:11 -0300

    feat: implement WizardService#findById
    Episode 29: https://www.youtube.com/watch?v=BM7TjjJinXA&list=PLqq9AhcMm2oPdXXFT3fzjaKLsVymvMXaY&index=29

---
+++ src/main/java/com/learningspring/hogwartsartifactonline/wizard/WizardService.java
@@ -0,0 +1,19 @@
+package com.learningspring.hogwartsartifactonline.wizard;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class WizardService {
+
+    private final WizardRepository wizardRepository;
+
+    public WizardService(WizardRepository wizardRepository) {
+        this.wizardRepository = wizardRepository;
+    }
+
+    public Wizard findById(Integer id) {
+        return this.wizardRepository.findById(id).get();
+    }
+}
