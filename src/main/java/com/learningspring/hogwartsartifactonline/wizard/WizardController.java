b299a86b941418c6b688256a8ac22a9c8899612c
Paulo Henrique <ph.1928@hotmail.com>
2024-09-12 09:50:05 -0300

    feat: implement WizardController#findWizardById
    Episode 29: https://www.youtube.com/watch?v=BM7TjjJinXA&list=PLqq9AhcMm2oPdXXFT3fzjaKLsVymvMXaY&index=29

--- src/main/java/com/learningspring/hogwartsartifactonline/wizard/WizardController.java
+++ src/main/java/com/learningspring/hogwartsartifactonline/wizard/WizardController.java
@@ -1,11 +1,31 @@
 package com.learningspring.hogwartsartifactonline.wizard;
 
+import com.learningspring.hogwartsartifactonline.system.Result;
+import com.learningspring.hogwartsartifactonline.system.StatusCode;
+import com.learningspring.hogwartsartifactonline.wizard.converter.WizardToWizardDtoConverter;
+import com.learningspring.hogwartsartifactonline.wizard.dto.WizardDto;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("apí/v1/wizards")
+@RequestMapping("/api/v1/wizards")
 public class WizardController {
 
+    private final WizardService wizardService;
+    private final WizardToWizardDtoConverter wizardToWizardDtoConverter;
 
+    public WizardController(WizardService wizardService,
+            WizardToWizardDtoConverter wizardToWizardDtoConverter) {
+        this.wizardService = wizardService;
+        this.wizardToWizardDtoConverter = wizardToWizardDtoConverter;
+    }
+
+    @GetMapping("/{wizardId}")
+    public Result findWizardById(@PathVariable Integer wizardId) {
+        Wizard foundWizard = this.wizardService.findById(wizardId);
+        WizardDto wizardDto = this.wizardToWizardDtoConverter.convert(foundWizard);
+        return new Result(true, StatusCode.SUCCESS, "Found One Success", wizardDto);
+    }
 }
