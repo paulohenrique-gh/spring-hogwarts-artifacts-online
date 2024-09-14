2e88aee5cf211c85aec622c13ec9a925249fac3e
Paulo Henrique <ph.1928@hotmail.com>
2024-09-14 09:09:02 -0300

    feat: implement WizardController#findAllWizards
    Episode 29: https://www.youtube.com/watch?v=BM7TjjJinXA&list=PLqq9AhcMm2oPdXXFT3fzjaKLsVymvMXaY&index=29

--- src/main/java/com/learningspring/hogwartsartifactonline/wizard/WizardController.java
+++ src/main/java/com/learningspring/hogwartsartifactonline/wizard/WizardController.java
@@ -9,11 +9,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/v1/wizards")
 public class WizardController {
 
     private final WizardService wizardService;
+
     private final WizardToWizardDtoConverter wizardToWizardDtoConverter;
 
     public WizardController(WizardService wizardService,
@@ -27,5 +30,14 @@ public class WizardController {
         Wizard foundWizard = this.wizardService.findById(wizardId);
         WizardDto wizardDto = this.wizardToWizardDtoConverter.convert(foundWizard);
         return new Result(true, StatusCode.SUCCESS, "Found One Success", wizardDto);
+    }
+
+    @GetMapping
+    public Result findAllWizards() {
+        List<WizardDto> wizardDtos = this.wizardService.findAll()
+                .stream()
+                .map(this.wizardToWizardDtoConverter::convert)
+                .toList();
+        return new Result(true, StatusCode.SUCCESS, "Find All Success", wizardDtos);
     }
 }
