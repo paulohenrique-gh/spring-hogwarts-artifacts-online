297423b0c86b4aa491c7ea44cf4c7b6de01ba6bb
Paulo Henrique <ph.1928@hotmail.com>
2024-11-06 14:53:40 -0300

    cd: setup mysql azure dabatase, and dev / prod environments, and azure key vault
    Episode 49: https://www.youtube.com/watch?v=BM7TjjJinXA&list=PLqq9AhcMm2oPdXXFT3fzjaKLsVymvMXaY&index=49

--- src/test/java/com/learningspring/hogwartsartifactonline/wizard/WizardControllerIntegrationTest.java
+++ src/test/java/com/learningspring/hogwartsartifactonline/wizard/WizardControllerIntegrationTest.java
@@ -16,6 +16,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultActions;
@@ -29,6 +30,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 @DisplayName("Integration tests for Wizard API endpoints")
 @Tag("integration")
+@ActiveProfiles(value = "dev")
 public class WizardControllerIntegrationTest {
 
     @Autowired
