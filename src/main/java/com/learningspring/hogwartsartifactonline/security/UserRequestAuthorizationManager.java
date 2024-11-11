ca8b842b320edabf24d37297aa5a924ea0dc66e4
Paulo Henrique <ph.1928@hotmail.com>
2024-11-11 11:45:35 -0300

    feat: restrict users to access only their own data
    Episode 57: https://www.youtube.com/watch?v=BM7TjjJinXA&list=PLqq9AhcMm2oPdXXFT3fzjaKLsVymvMXaY&index=58

---
+++ src/main/java/com/learningspring/hogwartsartifactonline/security/UserRequestAuthorizationManager.java
@@ -0,0 +1,42 @@
+package com.learningspring.hogwartsartifactonline.security;
+
+import org.springframework.security.authorization.AuthorizationDecision;
+import org.springframework.security.authorization.AuthorizationManager;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.web.access.intercept.RequestAuthorizationContext;
+import org.springframework.stereotype.Component;
+import org.springframework.web.util.UriTemplate;
+
+import java.util.Map;
+import java.util.function.Supplier;
+
+@Component
+public class UserRequestAuthorizationManager implements AuthorizationManager<RequestAuthorizationContext> {
+
+    private static final UriTemplate USER_URI_TEMPLATE = new UriTemplate("/users/{userId}");
+
+    @Override
+    public AuthorizationDecision check(Supplier<Authentication> authenticationSupplier, RequestAuthorizationContext context) {
+        // Extract the userId from the request URI: /users/{userId}
+        Map<String, String> uriVariables = USER_URI_TEMPLATE.match(context.getRequest().getRequestURI());
+        String userIdFromRequestUri = uriVariables.get("userId");
+
+        // Extract the userId from the authentication object, which is a JWT object
+        Authentication authentication = authenticationSupplier.get();
+        String userIdFromJwt = ((Jwt) authentication.getPrincipal()).getClaim("userId").toString();
+
+        // Check if the user has the role "ROLE_user"
+        boolean hasUserRole = authentication.getAuthorities().stream()
+                .anyMatch(grantedAuthority -> grantedAuthority.getAuthority().equals("ROLE_user"));
+
+        // Check if the user has the role "ROLE_admin"
+        boolean hasAdminRole = authentication.getAuthorities().stream()
+                .anyMatch(grantedAuthority -> grantedAuthority.getAuthority().equals("ROLE_admin"));
+
+        // Compare the two userIds
+        boolean userIdsMatch = userIdFromRequestUri != null && userIdFromRequestUri.equals(userIdFromJwt);
+
+        return new AuthorizationDecision(hasAdminRole || (hasUserRole && userIdsMatch));
+    }
+}
